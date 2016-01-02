0c7dd00ae00dc6f59eb86b57e2862c26550f0fb6
AmberGlasses <775837740@qq.com>
2016-01-02 11:36:13 +0800

    init

---
+++ inst/tests/test.chinaWeather.R
@@ -0,0 +1,15 @@
+library(testthat)
+context("filename: current of date")
+
+test_that("filename is current of date",{
+  daystr<-paste(format(Sys.Date(),"%Y%m%d"),".csv",sep="")
+  expect_that(filename(),equals(daystr))
+  
+  day<-as.Date("20110701",format="%Y%m%d")
+  expect_that(filename(day),equals("20110701.csv"))
+})
+
+test_that("filename is current of date, bad test",{
+  day<-as.Date("20110701",format("%Y%m%d"))
+  expect_that(filename(day),equals("20110702.csv"))
+})
